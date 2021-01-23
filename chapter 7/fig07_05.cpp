5aee395accbcee8f561249ef35c44b608942fa15
Lea Marsela <lea.marsela@gmail.com>
2021-01-23 11:58:25 +0700

    update chapter 7

---
+++ chapter 7/fig07_05.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <array>
+using namespace std;
+
+int	main(int argc, char **argv)
+{
+    const size_t arraySize = 5;
+
+    array<int, arraySize> s;
+
+    for (size_t i = 0; i < s.size(); ++i)
+    {
+        s[i] = 2 + 2 * i;
+    }
+    
+    cout << "Element" << setw(13) << "Value" << endl;
+
+    for (size_t j = 0; j < s.size(); ++j)
+    {
+        cout << setw(7) << j << setw(13) << s[j] << endl;
+    }
+    
+    system("pause");
+    return 0;
+}
---
+++ chapter 7/fig07_06.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int	main(int argc, char **argv)
+{
+    const int x = 7;
+
+    cout << "The calue of constant variable x is: " << x << endl;
+
+    system("pause");
+    return 0;
+}
